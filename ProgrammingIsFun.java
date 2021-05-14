ea9b57f9f02589c343e73ab075b29033a3844b6e
zmajiza <84088300+zmajiza@users.noreply.github.com>
2021-05-14 09:34:46 -0400

    Add files via upload

---
+++ ProgrammingIsFun.java
@@ -0,0 +1,10 @@
+//ProgrammingIsFun.java
+import javax.swing.*;
+public class ProgrammingIsFun
+{
+   public static void main(String[] args)
+	{
+	   JOptionPane.showMessageDialog(null,"Programming is Fun!");
+		System.exit(0);
+	}
+}
